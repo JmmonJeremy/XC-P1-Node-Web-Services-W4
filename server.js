355c10e6f243e9dc5779e0b5e8cfedbc72027f01
JmmonJeremy <suc19002@byui.edu>
2024-10-10 19:12:45 -0700

    Added changes to allow react frontend to work with it.

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app
       res.setHeader('Content-Type', 'application/json');
     }
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
